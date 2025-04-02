9c3ce7434c6168c0cec0d439a4272487306dd385
rafnaves <rafaelnavesdev@gmail.com>
2025-04-01 22:03:02 -0300

    Bando de Dados

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask
-from flask_sqlachemy import SQLAlchemy
-
+from models.user import User
+from database import db
 
 #__name__ = __main__
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
 
-tasks = []
-task_id_control = 1
+db.init_app(app)
 
 @app.route('/hello_world', methods=['GET'])
 def hello_world():
