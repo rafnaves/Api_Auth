61a2764b572d260c162fbfb3953eeb4b18d5cd77
rafnaves <rafaelnavesdev@gmail.com>
2025-04-11 15:02:26 -0300

    logout e cadastro de usuario

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 #__name__ = __main__
 app = Flask(__name__)
@@ -15,7 +15,7 @@ login_manager.init_app(app)
 # view login
 login_manager.login_view = 'login'
 
-login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
@@ -35,6 +35,27 @@ def login():
             return jsonify({"message": "Autenticação Realizada com sucesso"})
 
     return jsonify({"message": "Credenciais Inválidas"}), 400
+
+@app.route('/logout', methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+@app.route('/user', methods=["POST"])
+def create_user():
+
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuario cadastrado com sucesso"})
+
+    return jsonify({"message": "Dados invalidos"}), 400
 
 @app.route('/hello_world', methods=['GET'])
 def hello_world():
